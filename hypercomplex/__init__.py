07efe19d0baf4734273a533cedd873149e21e310
discretegames <discretizedgames@gmail.com>
2021-07-07 12:43:11 -0700

    Filled out init imports.

--- hypercomplex/__init__.py
+++ hypercomplex/__init__.py
@@ -1,1 +1,31 @@
 # TODO
+
+
+__all__ = """
+reals
+cayley_dickson_construction cd_construction
+cayley_dickson_algebra cd_algebra
+R Real CD1
+C Complex CD2
+Q Quaternion CD4
+O Octonion CD8
+S Sedenion CD16
+P Pathion CD32
+X Chingon CD64
+U Routon CD128
+V Voudon CD256
+""".split()
+
+from hypercomplex.hypercomplex import \
+    reals, \
+    cayley_dickson_construction, cd_construction, \
+    cayley_dickson_algebra, cd_algebra, \
+    R, Real, CD1,\
+    C, Complex, CD2, \
+    Q, Quaternion, CD4, \
+    O, Octonion, CD8, \
+    S, Sedenion, CD16, \
+    P, Pathion, CD32, \
+    X, Chingon, CD64, \
+    U, Routon, CD128, \
+    V, Voudon, CD256
